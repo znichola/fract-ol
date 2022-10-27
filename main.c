a32289b81c509b650519b4f12c36e26a021365ed
znichola <znichola@student.42lausanne.ch>
2022-10-28 00:22:43 +0200

    first working fractal + resizing code

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: znichola <znichola@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/26 17:57:33 by znichola          #+#    #+#             */
-/*   Updated: 2022/10/26 22:05:29 by znichola         ###   ########.fr       */
+/*   Updated: 2022/10/28 00:22:42 by znichola         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,20 +34,6 @@ void put_square(t_data *data, int w, int h, int x, int y)
 	}
 }
 
-
-// xend = x + r cos(angle) and yend = y + r sin(angle)
-// void put_circle(t_data *data, int radius, int x, int y)
-// {
-// 	for (int i = x; i < x+radius*2; i++)
-// 		for (int e = y; e < y+radius*2; e++)
-// 		{
-// 			if ()
-// 			{
-// 				my_mlx_pixel_put(data, i, e, 0x00FF0000);
-// 			}
-// 		}
-// }
-
 int	DrawCircle(t_data *data, int r, int x, int y)
 {
 	static const double PI = 3.1415926535;
@@ -69,33 +55,70 @@ int	main(void)
 	void	*mlx_win;
 	t_data	img;
 
-	// neede to intialise the display engine
-	mlx = mlx_init();
-	
-	// initialises a new windoes and places it inside thie pointer
-	mlx_win = mlx_new_window(mlx, WIDTH, HIGHT, "Hello world!");
+	t_complex	z;
+	z.a = 0.0;
+	z.b = 0.0;
 
-	// retraving the temporary buffer frame on wich we write before writing,
-	//	to fill with the image we want to display
-	//	it's only been initialised here.
+	t_complex	c;
+	c.a = 0.0;
+	c.b = 0.0;
+
+	double	a_min = -2.2;
+	double	a_max = .8;
+	double	b_min = fabs(a_min - a_max) * ((double)HIGHT / (double)WIDTH) / -2;
+	double	b_max = fabs(a_min - a_max) * ((double)HIGHT / (double)WIDTH) / 2;
+
+	// print_complex("power", set(z, c, 5));
+	// printf ("found depth {%d}\n", fractal_set(z, c, 10));
+	
+	// printf("%f\n", map(WIDTH/2+10, WIDTH, -2, 2));
+
+	// return (0);
+
+	
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, WIDTH, HIGHT, "Hello world!");
 	img.img = mlx_new_image(mlx, WIDTH, HIGHT);
-	
-	
-	/*
-	** After creating an image, we can call `mlx_get_data_addr`, we pass
-	** `bits_per_pixel`, `line_length`, and `endian` by reference. These will
-	** then be set accordingly for the *current* data address.
-	*/
-	// now we need to populate the image parameters using the function.
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 								&img.endian);
 	
-	// fuction that put's a particular pixel to the display buffer
+	// mandelbrot set
+	//	f(z) = z^2 + C
+	// z is a complex variable
+	// C in a complex constant
+	//
+	//	first we start with z = 0;
+	//	z1 = f(0)
+	//	z2 = f(z1)
+	//	z3 = f(z2)
+	//	...
+	//	if this sequence is bounded, that C is solid, part of the mandelbrot set
+	//	ie if the one of the first numbers is bigger than 2, we started with this
 	
-	// make a square, centred on the top right.
-	
-	// put_circle(&img, 100, WIDTH/2, HIGHT/2);
-	DrawCircle(&img, (HIGHT/2)-50, WIDTH/2, HIGHT/2);
+	// #FFF9F9
+	// #FFFAF3
+	// #FFFDF0
+	// 0xFFFFFFFFFFFFFF06
+	int	colour = 0XE2DED0;
+	for (int h = 0; h < HIGHT; h++)
+	{
+		for (int w = 0; w < WIDTH; w++)
+		{
+			c.a = map(w, WIDTH, a_min, a_max);
+			if (w == 0)
+				c.b = map(h, HIGHT, b_min, b_max);
+			int d = fractal_set(z, c, DEPTH);
+			// printf("% 3d ", d);
+			if (d == -1)
+				colour = 0X4E4F50;
+			else 
+				colour = 0XE2DED0;
+			my_mlx_pixel_put(&img, w, h, colour);
+		}
+		// printf("\n ----");
+	}
+
+	// my_mlx_pixel_put(&img, 50, 50, 0X4E4F50);
 
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
